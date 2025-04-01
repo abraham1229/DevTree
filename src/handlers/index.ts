9584663de15412b86d88032597d039122380cce4
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-03-31 18:21:22 -0600

    Se arregla problema de tipo Response

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,4 @@
-import {Request} from 'express'
+import {Request, Response} from 'express'
 import User from "../models/User"
 import slug from 'slug'
 import { validationResult } from 'express-validator'
@@ -6,7 +6,7 @@ import { checkPassword, hashPassword } from '../utils/auth'
 
 
 //Se tiene any y se debe de evitar porque se puede usar el valor que sea
-export const createAccount = async (req: Request, res) => {
+export const createAccount = async (req: Request, res: Response) => {
   
   const { email, password } = req.body
 
@@ -14,7 +14,8 @@ export const createAccount = async (req: Request, res) => {
   const userExists = await User.findOne({ email }) //Returns the first
   if (userExists) {
     const error = new Error('Correo ya registrado')
-    return res.status(409).json({error : error.message})
+    res.status(409).json({ error: error.message })
+    return
   }
 
   //Comprobacion de handle 
@@ -22,7 +23,8 @@ export const createAccount = async (req: Request, res) => {
   const handleExists = await User.findOne({handle})
   if (handleExists) {
     const error = new Error('Usuario no disponible')
-    return res.status(409).json({error : error.message})
+    res.status(409).json({ error: error.message })
+    return
   }
   
   const user = new User(req.body) 
@@ -35,7 +37,7 @@ export const createAccount = async (req: Request, res) => {
 }
 
 
-export const login = async (req: Request, res) => {
+export const login = async (req: Request, res: Response) => {
   
   const { email, password } = req.body
 
@@ -43,14 +45,16 @@ export const login = async (req: Request, res) => {
   const user = await User.findOne({ email }) //Returns the first
   if (!user) {
     const error = new Error('Este correo no esta vinculado a una cuenta')
-    return res.status(404).json({error : error.message})
+    res.status(404).json({ error: error.message })
+    return
   }
 
   // Verificar password
   const isPasswordCorrect = await checkPassword(password,user.password)
   if (!isPasswordCorrect) {
     const error = new Error('Password Incorrecto') 
-    return res.status(401).json({error: error.message})
+    res.status(401).json({error: error.message})
+    return
   }
   
   res.send('Autenticado')
