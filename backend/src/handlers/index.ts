bda34e5b2dc1616e11b52b16aec6e53aa59d64e6
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-02 08:54:51 -0600

    Se agrega autenticacion de usuario

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express'
 import User from "../models/User"
 import slug from 'slug'
+import jwt from 'jsonwebtoken'
 import { checkPassword, hashPassword } from '../utils/auth'
 import { generateJWT } from './jwt'
 
@@ -61,4 +62,41 @@ export const login = async (req: Request, res: Response) => {
   const token = generateJWT({id: user._id})
 
   res.send(token)
+}
+
+export const getUser = async (req: Request, res: Response) => {
+  const bearer = req.headers.authorization
+  // No se recibe bearer
+  if (!bearer) {
+    const error = new Error('Usuario no identificado')
+    res.status(401).json({ error: error.message })
+    return
+  }
+
+  const [, token] = bearer.split(' ')
+
+
+  if (!token) {
+    const error = new Error('Usuario no autorizado')
+    res.status(401).json({ error: error.message })
+    return
+  }
+  
+  try {
+    const result = jwt.verify(token, process.env.JWT_SECRET)
+    if (typeof result === 'object' && result.id) {
+      const user = await User.findById(result.id).select('name handle email')
+      if (!user) {
+        const error = new Error('El usuario no existe')
+        res.status(404).json({ error: error.message })
+        return
+      }
+      
+      res.json(user)
+    }
+  } catch (error) {
+    res.status(500).json({error: "Token no valido"})
+  }
+
+
 }
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
-import { createAccount, login } from './handlers'
+import { createAccount, getUser, login } from './handlers'
 import { handleInputErrors } from './middleware/validation'
 
 //Routes creation
@@ -32,5 +32,7 @@ router.post('/auth/login',
   handleInputErrors,
   login
 )
+
+router.get('/user', getUser)
 
 export default router
