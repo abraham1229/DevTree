5b6ae18f5dbf699aaf0359faed78fd13e235fb34
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-05 16:47:59 -0600

    Se agrega endpoint para actualizacion de usuarios

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,39 +1,40 @@
-import { Router } from 'express'
-import { body } from 'express-validator'
-import { createAccount, getUser, login } from './handlers'
-import { handleInputErrors } from './middleware/validation'
-import { authenticate } from './middleware/auth'
+import { Router } from "express";
+import { body } from "express-validator";
+import { createAccount, getUser, login, updateProfile } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
+import { authenticate } from "./middleware/auth";
 
 //Routes creation
-const router = Router()
+const router = Router();
 // Autenticacion y registro
-router.post('/auth/register',
-  body('handle')
-    .notEmpty()
-    .withMessage('El handle no puede ir vacio'),
-  body('name')
-    .notEmpty()
-    .withMessage('El nombre no puede ir vacio'),
-  body('email')
-    .isEmail()
-    .withMessage('E-mail no válido'), 
-  body('password')
+router.post(
+  "/auth/register",
+  body("handle").notEmpty().withMessage("El handle no puede ir vacio"),
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("email").isEmail().withMessage("E-mail no válido"),
+  body("password")
     .isLength({ min: 8 })
-    .withMessage('El password es muy corto, mínimo es 8 caracteres'),
+    .withMessage("El password es muy corto, mínimo es 8 caracteres"),
   handleInputErrors,
-  createAccount)
+  createAccount
+);
 
-router.post('/auth/login',
-  body('email')
-    .isEmail()
-    .withMessage('E-mail no válido'), 
-  body('password')
-    .notEmpty()
-    .withMessage('El password es obligatorio'),
+router.post(
+  "/auth/login",
+  body("email").isEmail().withMessage("E-mail no válido"),
+  body("password").notEmpty().withMessage("El password es obligatorio"),
   handleInputErrors,
   login
-)
+);
 
-router.get('/user', authenticate ,getUser)
+router.get("/user", authenticate, getUser);
+router.patch(
+  "/user",
+  body("handle").notEmpty().withMessage("El handle no puede ir vacio"),
+  body("description").notEmpty().withMessage("La descripcion no puede ir vacia"),
+  handleInputErrors,
+  authenticate,
+  updateProfile
+);
 
-export default router
+export default router;
