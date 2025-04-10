30786e8f16905dc5e1633042b55f8b7c4c66a62a
abraham1229 <abrahamortizcastro1229@gmail.com>
2025-04-10 16:53:25 -0600

    Se agrega visualizacion de links en perfil

--- frontend/src/components/DevTree.tsx
+++ frontend/src/components/DevTree.tsx
@@ -1,13 +1,24 @@
 import { Link, Outlet } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import NavigationTabs from './NavigationTabs'
-import { User } from '../types'
+import { SocialNetwork, User } from '../types'
+import { useEffect, useState } from 'react'
+import DevTreeLink from './DevTreeLink'
 
 type DevTreeProps = {
   data: User
 }
 
-export default function DevTree({data} : DevTreeProps) {
+export default function DevTree({ data }: DevTreeProps) {
+
+  const [enabledLinks, setEnabledLinks] = useState<SocialNetwork[]>(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
+  
+  useEffect(() => {
+    setEnabledLinks(JSON.parse(data.links).filter((item: SocialNetwork) => item.enabled))
+  
+  }, [data])
+  
+  
   return (
     <>
       <header className="bg-slate-800 py-5">
@@ -49,7 +60,16 @@ export default function DevTree({data} : DevTreeProps) {
                 <img src={data.image} alt='Imagen Perfil' className='mx-auto max-w-[250px]' />
               }
 
-              <p className='text-center text-lg font-black text-white'>{ data.description }</p>
+              <p className='text-center text-lg font-black text-white'>{data.description}</p>
+              
+              <div className='mt-20 flex flex-col gap-5'>
+                {enabledLinks.map(link => (
+                  <DevTreeLink
+                    key={link.name}
+                    link={link}
+                  />
+                ))}
+              </div>
 
             </div>
           </div>
